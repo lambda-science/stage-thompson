563f52f494f7b92589a31ee9907890a5ae68a11e
Corentin M <meyer.corentin@gmail.com>
2020-01-31 15:30:37 +0100

    prototype colocalization exon intron

--- bin/find_file.sh
+++ bin/find_file.sh
@@ -1,2 +1,2 @@
-grep -w "$1" /home/meyer/stage-thompson/raw/uniprot/*.id
+grep -w "$1" /home/meyer/stage-thompson/raw/uniprot-sequence/*.id
 grep -w "$1" /home/meyer/stage-thompson/raw/mismatch-error/uniprot_errors.txt
