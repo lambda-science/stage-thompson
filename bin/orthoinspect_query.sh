b63961f76e533c23c0ab725691e685cb7dfb24b7
Corentin Meyer <meyer.corentin@gmail.com>
2020-02-05 11:20:12 +0100

    Convertion of 2 notebooks to function + .py script

---
+++ bin/orthoinspect_query.sh
@@ -0,0 +1,7 @@
+taxonID=(60711 9541 9544 9555 9595 9598 9601 61853 9483 30611)
+name=(C.sabaeus M.fascicularis M.mulatta P.anubis G.gorilla H.sapiens P.troglodytes P.abellii N.leucogenys C.jacchus O.garnettii)
+human=9606
+for i in ${taxonID[@]}
+do
+    curl -X GET "https://lbgi.fr/orthoinspectorv3/api/Eukaryota/species/$human/orthologs/$i" -H  "accept: application/json" > ../raw/orthoinspector-json/$i.json
+done
