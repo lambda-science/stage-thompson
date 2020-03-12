4d14a045a84d8723636f603565c88b16325cf228
Corentin Meyer <meyer.corentin@gmail.com>
2020-03-12 12:11:45 +0100

    update

--- bin/error_count.sh
+++ bin/error_count.sh
@@ -1,5 +1,5 @@
-FILES=~/stage-thompson/data/raw/uniprot-sequence/*.mafft
+FILES=~/stage-thompson/data/raw/uniprot-sequence-v2/*.mafft
 for f in $FILES
 do
-	/home/julie/ALN_UTILS/seqerrs $f >> ~/stage-thompson/data/raw/uniprot_errors.txt
+	/home/julie/ALN_UTILS/seqerrs $f >> ~/stage-thompson/data/raw/uniprot_errors_V2.txt
 done
