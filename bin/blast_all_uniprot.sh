692a0289182b7a3502d947c2e2a63acbc768dee8
Corentin M <meyer.corentin@gmail.com>
2020-03-18 10:36:27 +0100

    cat

--- bin/blast_all_uniprot.sh
+++ bin/blast_all_uniprot.sh
@@ -2,6 +2,7 @@ FILES=~/stage-thompson/data/raw/uniprot-blast/*.fasta
 database="callithrix_jacchus_uni.fasta  chlorocebus_sabaeus_uni.fasta macaca_fascicularis_uni.fasta nomascus_leucogenys_uni.fasta  pan_troglodytes_uni.fasta pongo_abelii_uni.fasta gorilla_gorilla_uni.fasta macaca_mulatta_uni.fasta papio_anubis_uni.fasta otolemur_garnettii_uni.fasta"
 for f in $FILES
 do
+	cat $f >> $f.results
     for db in $database
 	do
 		/biolo/blast/bin/blastp -db /commun/bics/DB-Corentin/uniprot-per-primate/$db -evalue 0.005 -outfmt '6 sseqid sseq' -max_target_seqs 1 -query $f | awk 'BEGIN{FS="\t"; OFS="\n"}{gsub(/-/, "", $2); print ">"$1,$2}' >> $f.results
