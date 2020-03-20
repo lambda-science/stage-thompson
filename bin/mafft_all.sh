bfd3c2afb8dc9d15e9b55c6393d1012ad4346d3f
Corentin M <meyer.corentin@gmail.com>
2020-03-20 17:33:04 +0100

    update mafting

--- bin/mafft_all.sh
+++ bin/mafft_all.sh
@@ -1,4 +1,10 @@
-FILES=~/stage-thompson/data/raw/uniprot-blast/*.results
+FILES=~/stage-thompson/data/raw/uniprot-blast/results/*.fasta
+for f in $FILES
+do
+	/biolo/mafft/inst/bin/mafft $f > $f.mafft
+done
+
+FILES=~/stage-thompson/data/raw/refseq-blast/results/*.fasta
 for f in $FILES
 do
 	/biolo/mafft/inst/bin/mafft $f > $f.mafft
--- bin/mafft_all_ref.sh
+++
@@ -1,5 +0,0 @@
-FILES=~/stage-thompson/data/raw/refseq-blast/*.results
-for f in $FILES
-do
-	/biolo/mafft/inst/bin/mafft $f > $f.mafft
-done
