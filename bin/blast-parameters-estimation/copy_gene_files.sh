16ad2a1d295c4061fc5250d159d8b4128e6711c4
Corentin M <meyer.corentin@gmail.com>
2020-03-30 15:05:45 +0200

    fix

--- bin/blast-parameters-estimation/copy_gene_files.sh
+++ bin/blast-parameters-estimation/copy_gene_files.sh
@@ -1,3 +1,3 @@
 while read l; do
-  cp ../../data/raw/uniprot-blast/$l ../../data/raw/blastp-parameter-estimation§$l
+  cp ../../data/raw/uniprot-blast/$l ../../data/raw/blastp-parameter-estimation/$l
 done < ../../data/raw/500_genes_samples.txt
