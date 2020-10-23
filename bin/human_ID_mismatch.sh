83107ecb80bdca06f75a49fe032c28055d0523f8
Corentin Meyer <meyer.corentin@gmail.com>
2020-10-23 11:25:59 +0200

    Préparation stage Selim

--- bin/human_ID_mismatch.sh
+++ bin/human_ID_mismatch.sh
@@ -1,3 +1,5 @@
+# Récupère les erreur et sort les protéines humainie créant un mismatch dans data/mismatch-flagging/human_uniprot_ID.id
+# Exécute ensuite le script Uniprot_To_Ensembl_1_1.py pour récupérer les identifiants ensembl des transcripts
 grep "SEQ_ERROR3" data/raw/uniprot_new_errors.txt | grep ".*.id" -o | \
 sed "s/\/home\/meyer\/uniprot\/\(.*\)\.id/\1/" | uniq \
 > data/mismatch-flagging/human_uniprot_ID.id
--- bin/retrieve_seq_isoform.sh
+++ bin/retrieve_seq_isoform.sh
@@ -1,2 +1,3 @@
 FILES=~/stage-thompson/temp/isoform/all_isoform.id
 /biolo/blast/bin/blastdbcmd -entry_batch $FILES -db /commun/bics/DB-Corentin/uniprot >> $FILES.fasta
+cat temp/isoform/*.fasta > temp/isoform/final_all_isoform.fasta
